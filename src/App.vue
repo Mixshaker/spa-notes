6c1c931c3d38c1b0ccc9ca3981079af3e9bf4e51
Rostislav <rostislav.lobanok@gmail.com>
2020-05-21 23:46:07 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 40px;
+}
+
+.logo{
+  box-sizing: border-box;
+  width: 80px;
+}
+
+.container{
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
